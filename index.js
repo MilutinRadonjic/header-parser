e5ec3fdbe47b534770a5a1caa521f0cd2a99738d
Milutin R <milutin.radonjic@limitforex.com>
2018-07-05 14:08:26 +0200

    added start script

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.use(useragent.express());
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
